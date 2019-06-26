519ebf86c62042e7972b8935ccd7bcc62337b90b
childressdev <jcampbell@childressagency.com>
2019-06-26 15:06:19 -0400

    added fallback menu stuff

--- wp-theme-files/functions.php
+++ wp-theme-files/functions.php
@@ -14,8 +14,8 @@ function jquery_cdn(){
   }
 }
 
-add_action('wp_enqueue_scripts', 'cai_scripts');
-function cai_scripts(){
+add_action('wp_enqueue_scripts', 'maxvelocity_scripts');
+function maxvelocity_scripts(){
   wp_register_script(
     'bootstrap-popper',
     'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
@@ -33,7 +33,7 @@ function cai_scripts(){
   );
 
   wp_register_script(
-    'cai-scripts',
+    'maxvelocity-scripts',
     get_stylesheet_directory_uri() . '/js/custom-scripts.min.js',
     array('jquery', 'bootstrap-scripts'),
     '',
@@ -42,11 +42,11 @@ function cai_scripts(){
 
   wp_enqueue_script('bootstrap-popper');
   wp_enqueue_script('bootstrap-scripts');
-  wp_enqueue_script('cai-scripts');
+  wp_enqueue_script('maxvelocity-scripts');
 }
 
-add_filter('script_loader_tag', 'cai_add_script_meta', 10, 2);
-function cai_add_script_meta($tag, $handle){
+add_filter('script_loader_tag', 'maxvelocity_add_script_meta', 10, 2);
+function maxvelocity_add_script_meta($tag, $handle){
   switch($handle){
     case 'jquery':
       $tag = str_replace('></script>', ' integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>', $tag);
@@ -64,11 +64,11 @@ function cai_add_script_meta($tag, $handle){
   return $tag;
 }
 
-add_action('wp_enqueue_scripts', 'cai_styles');
-function cai_styles(){
+add_action('wp_enqueue_scripts', 'maxvelocity_styles');
+function maxvelocity_styles(){
   wp_register_style(
     'google-fonts',
-    'https://fonts.googleapis.com/css?family=Maitree:400,700|Nunito+Sans:400,600,700|Nunito:700'
+    'https://fonts.googleapis.com/css?family=Open+Sans:400,600,600i,700,800|Teko:400,700'
   );
 
   wp_register_style(
@@ -77,17 +77,17 @@ function cai_styles(){
   );
 
   wp_register_style(
-    'cai-css',
+    'maxvelocity-css',
     get_stylesheet_directory_uri() . '/style.css'
   );
 
   wp_enqueue_style('google-fonts');
   wp_enqueue_style('fontawesome');
-  wp_enqueue_style('cai-css');
+  wp_enqueue_style('maxvelocity-css');
 }
 
-add_filter('style_loader_tag', 'cai_add_css_meta', 10, 2);
-function cai_add_css_meta($link, $handle){
+add_filter('style_loader_tag', 'maxvelocity_add_css_meta', 10, 2);
+function maxvelocity_add_css_meta($link, $handle){
   switch($handle){
     case 'fontawesome':
       $link = str_replace('/>', ' integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">', $link);
@@ -97,19 +97,63 @@ function cai_add_css_meta($link, $handle){
   return $link;
 }
 
-add_action('after_setup_theme', 'cai_setup');
-function cai_setup(){
+add_action('after_setup_theme', 'maxvelocity_setup');
+function maxvelocity_setup(){
   add_theme_support('post-thumbnails');
   //set_post_thumbnail_size(320, 320);
 
   register_nav_menus(array(
     'header-nav' => 'Header Navigation',
-    'footer-nav' => 'Footer Navigation',
-    'company-menu' => 'Company Footer Menu',
-    'services-menu' => 'Services Footer Menu'
+    'footer-nav-1' => 'Footer Navigation 1',
+    'footer-nav-2' => 'Footer Navigation 2',
+    'footer-nav-3' => 'Footer Navigation 3',
+    'footer-nav-4' => 'Footer Navigation 4'
   ));
 
-  load_theme_textdomain('cai', get_stylesheet_directory_uri() . '/languages');
+  load_theme_textdomain('maxvelocity', get_stylesheet_directory_uri() . '/languages');
 }
 
 require_once dirname(__FILE__) . '/includes/class-wp-bootstrap-navwalker.php';
+
+function maxvelocity_get_menu_by_location($location){
+  if(empty($location)){ return false; }
+
+  $locations = get_nav_menu_locations();
+  if(!isset($locations[$location])){ return false; }
+
+  $menu_obj = get_term($locations[$location], 'nav_menu');
+  return $menu_obj;
+}
+
+function maxvelocity_header_fallback_menu(){ ?>
+  <div id="navbar" class="collapse navbar-collapse">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item<?php if(is_front_page()){ echo ' active'; } ?>">
+        <a href="<?php echo esc_url(home_url()); ?>" class="nav-link"><?php echo esc_html__('Home', 'maxvelocity'); ?></a>
+      </li>
+      <li class="nav-item<?php if(is_page('classes') || is_singular('class')){ echo ' active'; } ?>">
+        <a href="<?php echo esc_url(home_url('tactical-classes')); ?>" class="nav-link"><?php echo esc_html__('Training', 'maxvelocity'); ?></a>
+      </li>
+      <li class="nav-item<?php if(is_page('store') || is_woocommerce()){ echo ' active'; } ?>">
+        <a href="<?php echo esc_url(home_url('store')); ?>" class="nav-link"><?php echo esc_html__('Store', 'maxvelocity'); ?></a>
+      </li>
+      <li class="nav-item<?php if(is_home() || is_singular('post')){ echo ' active'; } ?>">
+        <a href="<?php echo esc_url(home_url('blog')); ?>" class="nav-link"><?php echo esc_html__('Blog', 'maxvelocity'); ?></a>
+      </li>
+      <li class="nav-item<?php if(is_page('forum')){ echo ' active'; } ?>">
+        <a href="<?php echo esc_url(home_url('forum')); ?>" class="nav-link"><?php echo esc_html__('Forum', 'maxvelocity'); ?></a>
+      </li>
+      <li class="nav-item<?php if(is_page('videos')){ echo ' active'; } ?>">
+        <a href="<?php echo esc_url(home_url('videos')); ?>" class="nav-link"><?php echo esc_html__('Videos', 'maxvelocity'); ?></a>
+      </li>
+      <li class="nav-item<?php if(is_page('payments')){ echo ' active'; } ?>">
+        <a href="<?php echo esc_url(home_url('payments')); ?>" class="nav-link"><?php echo esc_html__('Payments', 'maxvelocity'); ?></a>
+      </li>
+      <li class="nav-item<?php if(is_page('contact-us')){ echo ' active'; } ?>">
+        <a href="<?php echo esc_url(home_url('contact-us')); ?>" class="nav-link"><?php echo esc_html__('Contact Us', 'maxvelocity'); ?></a>
+      </li>
+    </ul>
+  </div>
+<?php }
+
+require_once dirname(__FILE__) . '/includes/woo-functions.php';
