43e291affe7822d403b47b54a0a9aeb1ec118c78
childressdev <jcampbell@childressagency.com>
2019-06-27 11:23:29 -0400

    added excerpt

--- wp-theme-files/partials/loop-general_item.php
+++ wp-theme-files/partials/loop-general_item.php
@@ -8,5 +8,8 @@
 <div class="course-loop">
   <div class="course-short">
     <h3><a href="<?php the_permalink(); ?>" class="course-title-link"><?php the_title(); ?></a></h3>
+    <div class="course-excerpt">
+      <?php the_excerpt(); ?>
+    </div>
   </div>
 </div>
