00bd8072b8d981a1630c10b295cbc581cbe00a04
childressdev <jcampbell@childressagency.com>
2019-07-01 17:37:57 -0400

    added missing translation

--- wp-theme-files/partials/loop-post_item.php
+++ wp-theme-files/partials/loop-post_item.php
@@ -8,6 +8,6 @@
 <div class="post-link" style="background-image:url(<?php echo esc_url(get_the_post_thumbnail_url('full')); ?>); background-position:center center;">
   <div class="post-link-content d-flex flex-column justify-content-between">
     <h3><?php the_title(); ?></h3>
-    <a href="<?php the_permalink(); ?>" class="btn-alt align-self-center">READ MORE</a>
+    <a href="<?php the_permalink(); ?>" class="btn-alt align-self-center"><?php echo esc_html__('READ MORE', 'maxvelocity'); ?></a>
   </div>
 </div>
