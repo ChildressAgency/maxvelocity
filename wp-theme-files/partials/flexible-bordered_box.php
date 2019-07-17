857b129bcdd428faaa14b98425ed8e4e7dc6e0f9
childressdev <jcampbell@childressagency.com>
2019-07-17 17:48:43 -0400

    fix typo

--- wp-theme-files/partials/flexible-bordered_box.php
+++ wp-theme-files/partials/flexible-bordered_box.php
@@ -8,7 +8,7 @@
 
 <section class="container">
   <div class="box-callout">
-    <h3 class="box-callout-title"><?php echo esc_html(get_sub_field('border_box_title'); ?></h3>
+    <h3 class="box-callout-title"><?php echo esc_html(get_sub_field('border_box_title')); ?></h3>
     <div class="box-callout-body">
       <?php echo apply_filters('the_content', wp_kses_post(get_sub_field('bordered_box_content'))); ?>
     </div>
