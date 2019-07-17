aeb9b3c19fbeba083e33b346098a45a2a957d736
childressdev <jcampbell@childressagency.com>
2019-07-17 17:49:37 -0400

    added conditional check for link

--- wp-theme-files/partials/flexible-more_info_links.php
+++ wp-theme-files/partials/flexible-more_info_links.php
@@ -11,13 +11,17 @@
     <?php $left_side_iamge = get_sub_field('left_side_image'); ?>
     <div class="col-md-6 d-flex justify-content-center align-items-center" style="background-image:url(<?php echo esc_url($left_side_iamge['url']); ?>); background-position:center center;">
       <?php $left_side_link = get_sub_field('left_side_link'); ?>
-      <a href="<?php echo esc_url($left_side_link['url']); ?>"><?php echo esc_html($left_side_link['title']); ?></a>
+      <?php if($left_side_link): ?>
+        <a href="<?php echo esc_url($left_side_link['url']); ?>"><?php echo esc_html($left_side_link['title']); ?></a>
+      <?php endif; ?>
       <div class="dark-overlay"></div>
     </div>
     <?php $right_side_image = get_sub_field('right_side_image'); ?>
     <div class="col-md-6 d-flex justify-content-center align-items-center" style="background-image:url(<?php echo esc_url($right_side_image['url']); ?>); background-position:center center;">
       <?php $right_side_link = get_sub_field('right_side_link'); ?>
-      <a href="<?php echo esc_url($right_side_link['url']); ?>"><?php echo esc_html($right_side_link['title']); ?></a>
+      <?php if($right_side_link): ?>
+        <a href="<?php echo esc_url($right_side_link['url']); ?>"><?php echo esc_html($right_side_link['title']); ?></a>
+      <?php endif; ?>
       <div class="dark-overlay"></div>
     </div>
   </div>
