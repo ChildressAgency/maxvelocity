0ca11a2c5bf3a40539ffe9db73e014e243841ac0
childressdev <jcampbell@childressagency.com>
2019-07-18 18:05:29 -0400

    added shop loop start and end

--- wp-theme-files/includes/woo-functions.php
+++ wp-theme-files/includes/woo-functions.php
@@ -3,6 +3,7 @@ if(!defined('ABSPATH')){ exit; }
 
 remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
 
 add_action('woocommerce_before_main_content', 'maxvelocity_wrapper_start', 10);
 function maxvelocity_wrapper_start(){
@@ -16,14 +17,22 @@ function maxvelocity_wrapper_end(){
   echo '</article</div></main>';
 }
 
-add_action('after_theme_setup', 'maxvelocity_add_woocommerce_support');
-function maxvelocity_add_woocommerce_support(){
-  add_theme_support('woocommerce');
-  add_theme_support('wc-product-gallery-zoom');
-  add_theme_support('wc-product-gallery-lightbox');
-  add_theme_support('wc-product-gallery-slider');
+add_action('woocommerce_before_shop_loop', 'maxvelocity_shop_loop_wrapper_open', 15);
+function maxvelocity_shop_loop_wrapper_open(){
+  echo '<div class="row">
+          <div class="col-md-8 col-lg-9 order-md-12">';
 }
 
+add_action('woocommerce_after_shop_loop', 'maxvelocity_shop_loop_wrapper_close', 15);
+function maxvelocity_shop_loop_wrapper_close(){
+  echo '</div>'; //close col-md-8
+  echo '<div class="col-md-4 col-lg-3 order-md-1">';
+    get_sidebar('shop');
+  echo '</div>';
+  echo '</div>'; //close row
+}
+
+//header cart
 add_action('maxvelocity_show_cart_link', 'maxvelocity_cart_link');
 function maxvelocity_cart_link(){
   if(in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))){
