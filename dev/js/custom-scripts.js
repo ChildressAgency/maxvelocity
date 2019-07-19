0ec165effef756b96435933431a80949c3250496
childressdev <jcampbell@childressagency.com>
2019-07-19 17:01:34 -0400

    fixed relatedTarget call

--- dev/js/custom-scripts.js
+++ dev/js/custom-scripts.js
@@ -63,9 +63,10 @@ jQuery(document).ready(function($){
   //reviews tab ajax
   $('.tab-changer').on('hide.bs.tab', function(e){
     $('.student-review').html('<p class="loading">Loading...</p>');
-    
-    var reviewId = e.relatedTarget.data('review_id');
-    var nonce = e.relatedTarget.data('nonce');
+
+    var $newTab = e.relatedTarget;
+    var reviewId = $($newTab).data('review_id');
+    var nonce = $($newTab).data('nonce');
 
     var reviewData = {
       'action': 'maxvelocity_get_review',
@@ -79,29 +80,29 @@ jQuery(document).ready(function($){
   });
 });
 
-$.fn.carouselHeights = function () {
+$.fn.carouselHeights = function(){
   var items = $(this), //grab all slides
     heights = [], //create empty array to store height values
     tallest; //create variable to make note of the tallest slide
 
-  var normalizeHeights = function () {
-    items.each(function () { //add heights to array
+  var normalizeHeights = function(){
+    items.each(function(){ //add heights to array
       heights.push($(this).height());
     });
     tallest = Math.max.apply(null, heights); //cache largest value
-    items.each(function () {
+    items.each(function(){
       $(this).css('min-height', tallest + 'px');
     });
   };
 
   normalizeHeights();
 
-  $(window).on('resize orientationchange', function () {
+  $(window).on('resize orientationchange', function(){
     //reset vars
     tallest = 0;
     heights.length = 0;
 
-    items.each(function () {
+    items.each(function(){
       $(this).css('min-height', '0'); //reset min-height
     });
     normalizeHeights(); //run it again 
