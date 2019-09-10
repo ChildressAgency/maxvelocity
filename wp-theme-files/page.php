0403e22d6bf188bc4f7575107f234910ea1b4970
childressdev <jcampbell@childressagency.com>
2019-09-10 09:13:25 -0400

    removed default center alignment

--- wp-theme-files/page.php
+++ wp-theme-files/page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <main id="main">
   <div class="container">
-    <article class="intro-centered">
+    <article>
       <?php
         get_template_part('partials/section', 'page_title');
         
